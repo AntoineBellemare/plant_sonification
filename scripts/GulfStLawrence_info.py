5c38d5a6f19f13091e3cba9d8b63f3123877d998
AntoineBellemare <antoine.bellemare9@gmail.com>
2025-04-23 10:08:48 -0400

    info on datasets

---
+++ scripts/GulfStLawrence_info.py
@@ -0,0 +1,24 @@
+# captation_series.py
+
+SPRING = 'Serie 6'
+SUMMER = 'Serie 23'
+FALL = 'Serie 37'
+WINTER = 'Serie 38'
+
+seasons = {
+    SPRING: 'Spring 2024',
+    SUMMER: 'Summer 2024',
+    FALL: 'Fall 2024',
+    WINTER: 'Winter 2025',
+}
+
+plant_common_names = {
+    'Amelanchier canadensis': 'Serviceberry',
+    'Dryopteris filix-mas': 'Male Fern',
+    'Lysimachia ciliata': 'Fringed Loosestrife',
+    'Onoclea sensibilis': 'Sensitive Fern',
+    'Diervilla lonicera': 'Bush Honeysuckle',
+    'Carex intumescens': 'Bladder Sedge',
+    'Vaccinium angustifolium': 'Lowbush Blueberry',
+    'Symphiotrum novi-belgii': 'New York Aster',
+}
---
+++ scripts/LaurentianMapleGrove_info.py
@@ -0,0 +1,25 @@
+# captation_series.py
+
+SPRING = 'Serie 3'
+SUMMER = 'Serie 29'
+FALL = 'Serie 36'
+WINTER = 'Serie 2'
+
+seasons = {
+    SPRING: 'Spring 2024',
+    SUMMER: 'Summer 2024',
+    FALL: 'Fall 2024',
+    WINTER: 'Winter 2024',
+}
+
+plant_common_names = {
+    'Acer rubrum': 'Red Maple',
+    'Rhododendron groenlandicum': 'Labrador Tea',
+    'Dryopteris mas': 'Male Fern',
+    'Polystichum acrostichoides': 'Christmas Fern',
+    'Alnus': 'Alder',
+    'Geum rivale': 'Water Avens',
+    'Myrica gale': 'Sweetgale',
+    'Acer pensylvanicum': 'Striped Maple',
+}
+
